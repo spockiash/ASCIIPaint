a9c121f985cb9e386bcb98628d7d594f4db75d96
Bobo <theb02b04@gmail.com>
2024-10-29 23:35:11 +0100

    circle tool prototype added

--- src/draw_canvas/tool_base.hpp
+++ src/draw_canvas/tool_base.hpp
@@ -6,11 +6,13 @@
 namespace draw_canvas {
     class ToolBase {
     public:
-        ToolBase(int header_offset = 1, int tool_offset = 0, int border_offset = 1)
+        virtual ~ToolBase() = default;
+
+        explicit ToolBase(const int header_offset = 1, const int tool_offset = 0, const int border_offset = 1)
             : HEADER_OFFSET(header_offset), TOOL_OFFSET(tool_offset), BORDER_OFFSET(border_offset) {}
         virtual bool HandleEvent(const ftxui::Mouse& mouse, Canvas& canvas) = 0;
-        virtual const char* GetName() const = 0;
-        virtual void SetCharacter(char selected_char)
+        [[nodiscard]] virtual const char* GetName() const = 0;
+        virtual void SetCharacter(const char selected_char)
         {
             this->selected_char = selected_char;
         };
